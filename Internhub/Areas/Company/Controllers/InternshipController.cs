8af61eea2029d49be5c75d8b14c8daafa52fede0
PhilsKay <philipinhokayode@gmail.com>
2022-07-26 04:06:26 +0100

    added some pages to company

---
+++ Internhub/Areas/Company/Controllers/InternshipController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Internhub.Areas.Company.Controllers
+{
+    [Area("Company")]
+    public class InternshipController : Controller
+    {
+        private readonly ILogger<InternshipController> _logger;
+
+        public InternshipController(ILogger<InternshipController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Post()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Resumes()
+        {
+            return View();
+        }
+    }
+}
