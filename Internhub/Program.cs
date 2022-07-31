093f5222a3e3b4a486270e8f9766a503d00786d7
PhilsKay <philipinhokayode@gmail.com>
2022-08-01 00:09:06 +0100

    addded Session and state management configuration

--- Internhub/Program.cs
+++ Internhub/Program.cs
@@ -10,6 +10,15 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(
     builder.Configuration.GetConnectionString("DefaultConnection")));
 
+//Add service for session
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +35,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.UseEndpoints(endpoints =>
 {
--- Internhub/Utilities/SessionExtensions.cs
+++ Internhub/Utilities/SessionExtensions.cs
@@ -1,6 +1,20 @@
-﻿namespace Internhub.Utilities
+﻿
+using System.Text.Json;
+
+namespace Internhub.Utilities
 {
-    public class SessionExtensions
+    public static class SessionExtensions
     {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default : JsonSerializer.Deserialize<T>(value);
+        }
+
     }
 }
